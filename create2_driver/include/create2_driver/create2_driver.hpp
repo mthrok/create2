8325c1ce22595e4a80569e2ea9dfd86c6c8c9fc3
moto <mthrok@gmail.com>
2016-01-07 10:36:14 -0800

    Create Communicator class for multithreading serial IO

--- create2_driver/include/create2_driver/create2_driver.hpp
+++ create2_driver/include/create2_driver/create2_driver.hpp
@@ -5,11 +5,33 @@
 #include "create2_driver/types.hpp"
 #include "create2_driver/serial.hpp"
 
-
 using namespace create2;
 
-class Create2 {
-  Serial comm_;
+namespace create2 {
+  class Communicator;
+  class Create2;
+};
+
+class create2::Communicator {
+  Serial serial_;
+
+public:
+  Communicator();
+  ~Communicator();
+
+  void init(const int baudrate=B115200,
+            const char* device="/dev/ttyUSB0");
+  void start();
+
+  void sendOpCode(const OPCODE code);
+  void sendOpCode(const OPCODE code,
+                  const uint8_t* pData,
+                  const uint32_t size);
+
+};
+
+class create2::Create2 {
+  Communicator comm_;
 
 public:
   Create2();
@@ -17,8 +39,6 @@ public:
 
   void init(const int baudrate=B115200,
             const char* device="/dev/ttyUSB0");
-
-  void sendOpCode(const OPCODE code, const uint32_t size, const uint8_t* pData);
 
   void test();
 };
--- create2_driver/src/create2_driver.cxx
+++ create2_driver/src/create2_driver.cxx
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "create2_driver/create2_driver.hpp"
 
 void sleep_for_sec (float sec) {
@@ -5,27 +6,59 @@ void sleep_for_sec (float sec) {
   usleep(usec);
 }
 
-Create2::Create2()
-  : comm_()
+create2::Communicator::Communicator()
+  : serial_()
+{}
+
+create2::Communicator::~Communicator()
 {};
 
-Create2::~Create2() {};
+void create2::Communicator::init(
+    const int baudrate, const char* device) {
+  serial_.init(baudrate, device);
+}
 
-void Create2::init(const int baudrate, const char* device) {
-  comm_.init(baudrate, device);
-};
-
-void Create2::sendOpCode(const OPCODE code, const uint32_t size, const uint8_t* pData) {
+void create2::Communicator::sendOpCode(
+    const OPCODE code, const uint8_t* pData, const uint32_t size) {
   uint8_t* pBuffer = new uint8_t[size + 1];
   pBuffer[0] = (uint8_t)code;
   memcpy(pBuffer+1, pData, size);
-  if (comm_.write(pBuffer, size+1) != (int32_t)(size + 1)) {
-    throw std::runtime_error("Failed to send op code.");
+  if (serial_.write(pBuffer, size+1) != (int32_t)(size + 1)) {
+    std::cerr << "Failed to send Opcode: " << code << std::endl;
+  }
+  delete[] pBuffer;
+}
+
+void create2::Communicator::sendOpCode(const OPCODE code) {
+  uint8_t c = (uint8_t)code;
+  if (serial_.write(&c, 1) != 1) {
+    std::cerr << "Failed to send Opcode: " << code << std::endl;
   }
 }
 
-void Create2::test() {
-  sendOpCode(OC_START, 0, NULL);
-  sleep_for_sec(2);
-  //sendOpCode(OC_FULL, 0, NULL);
+create2::Create2::Create2()
+  : comm_()
+{};
+
+create2::Create2::~Create2() {};
+
+void create2::Create2::init(const int baudrate, const char* device) {
+  comm_.init(baudrate, device);
+};
+
+void create2::Create2::test() {
+  comm_.sendOpCode(OC_START);
+  sleep_for_sec(1);
+
+  std::cout << "[TEST] Entering FULL mode." << std::endl;
+  comm_.sendOpCode(OC_FULL);
+  sleep_for_sec(1);
+
+  std::cout << "[TEST] Entering PASSIVE mode." << std::endl;
+  comm_.sendOpCode(OC_START);
+  sleep_for_sec(1);
+
+  std::cout << "[TEST] Restarting." << std::endl;
+  comm_.sendOpCode(OC_RESET);
+  sleep_for_sec(5);
 }
